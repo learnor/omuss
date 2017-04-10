1fc1533eb4a86924378ad50a408fd0e83e13241e
zhengzhi2936 <zhengzhi2616@gmail.com>
2017-04-10 15:25:10 +0800

    add bootstarap&flash&navbar&group_model to this app

---
+++ db/migrate/20170410071519_create_groups.rb
@@ -0,0 +1,12 @@
+class CreateGroups < ActiveRecord::Migration[5.0]
+  def change
+    create_table :groups do |t|
+      t.string :title
+      t.text :description
+      t.string :room
+      t.string :password
+
+      t.timestamps
+    end
+  end
+end
