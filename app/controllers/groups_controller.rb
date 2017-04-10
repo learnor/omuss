b506ca057f194bbf88be15c3dcf77c65ed2e1ec2
zhengzhi2936 <zhengzhi2616@gmail.com>
2017-04-10 15:44:13 +0800

    add views to this app

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,49 @@
 class GroupsController < ApplicationController
 
+  before_action :find_group, except: [:index, :new, :create]
   def index
-    @group = Group.all
+    @groups = Group.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to "/"
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @group.update(group_params)
+      redirect_to "/"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @group.destroy
+    redirect_to "/"
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:title, :description, :room, :password)
+  end
+
+  def find_group
+    @group = Group.find(params[:id])
   end
 end
